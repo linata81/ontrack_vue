b65f69579b20e7e49ec6fd81ae527b230c008b8d
Natalia Lysak <linata81@gmail.com>
2023-06-26 12:29:44 +0300

    splitting the interface into components

---
+++ src/components/TheNav.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+
+// const navItems = ['timeline', 'activities', 'progress']
+
+const navItems = {
+  timeline: ClockIcon,
+  activities: ListBulletIcon,
+  progress: ChartBarIcon
+}
+</script>
+
+<template>
+  <nav class="sticky bottom-0 z-10 bg-white">
+    <ul class="flex items-center justify-around border-t">
+      <li v-for="icon, page in navItems" :key="page" class="flex-1">
+        <a :href="`#${page}`" class="flex flex-col items-center p-2 text-xs capitalize">
+
+          <!-- 2вариант вывода иконок -->
+          <component :is="icon" class="h-6 w-6" />
+
+          <!-- 1 вариант вывода иконок -->
+          <!-- <ClockIcon v-if="page === 'timeline'" class="h-6 w-6"/>
+            <ListBulletIcon v-else-if="page === 'activities'" class="h-6 w-6"/>
+            <ChartBarIcon v-else class="h-6 w-6"/> -->
+
+          {{ page }}
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
