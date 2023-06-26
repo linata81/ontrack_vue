bcb11320396cf4d1950d6923bd36f536bca779d6
Natalia Lysak <linata81@gmail.com>
2023-06-26 12:49:05 +0300

    added component NavItem

---
+++ src/components/NavItem.vue
@@ -0,0 +1,16 @@
+<!-- для настройки передачи атрибута setup не должен быть -->
+<!-- отключаем наследование атрибута корневым элементом компонента, т.к. мы явно указали (v-bind="$attrs") что все атрибуты будут
+унаследованы элементом ссылки -->
+<script>
+export default {
+  inheritAttrs: false
+}
+</script>
+
+<template>
+  <li class="flex-1">
+    <a v-bind="$attrs" class="flex flex-col items-center p-2 text-xs capitalize">
+      <slot></slot>
+    </a>
+  </li>
+</template>
