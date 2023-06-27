c71ddf39c355b2ae0f7f200fee8f6a7842a542be
Natalia Lysak <linata81@gmail.com>
2023-06-27 10:04:57 +0300

    added active class menuItem

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,5 +1,6 @@
 <script setup>
-import NavItem from './NavItem.vue';
+import {ref} from 'vue'
+import NavItem from './NavItem.vue'
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 
 const navItems = {
@@ -7,12 +8,20 @@ const navItems = {
   activities: ListBulletIcon,
   progress: ChartBarIcon
 }
+
+const currentPage = ref('timeline')
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white">
     <ul class="flex items-center justify-around border-t">
-      <NavItem v-for="icon, page in navItems" :key="page" :href="`#${page}`">
+      <NavItem
+        v-for="icon, page in navItems"
+        :key="page"
+        :href="`#${page}`"
+        :class="{'bg-gray-200 pointer-events-none': page === currentPage}"
+        @click="currentPage = page"
+      >
         <component :is="icon" class="h-6 w-6"/> {{ page }}
       </NavItem>
     </ul>
