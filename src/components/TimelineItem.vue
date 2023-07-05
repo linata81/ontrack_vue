5f8e47d91837fbd2c75ccdee2511d963da323837
Natalia Lysak <linata81@gmail.com>
2023-07-05 09:29:19 +0300

    splitting into components button and select

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { XMarkIcon } from '@heroicons/vue/24/outline'
+import BaseSelect from './BaseSelect.vue';
 
 const props = defineProps(['timelineItem'])
 
@@ -15,22 +15,13 @@ const options = [
   {value: 2, label: 'Чтение'},
   {value: 3, label: 'Тренировка'}
 ]
+
+const selectedActivityId = 2
 </script>
 
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
-    <a href="#"
-      :class="hourLinkClasses">
-      {{ timelineItem.hour }}:00
-    </a>
-    <div class="flex gap-2">
-      <button class="rounded bg-gray-100 p-3 enabled:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50">
-        <XMarkIcon class="h-8" />
-      </button>
-      <select name="" id="" class="w-full truncate rounded bg-gray-100 py-1 px-2 text-2xl">
-        <option selected disabled value="" class>Отдых</option>
-        <option v-for="{value, label} in options" :key="value" :value="value">{{ label }}</option>
-      </select>
-    </div>
+    <a href="#" :class="hourLinkClasses"> {{ timelineItem.hour }}:00 </a>
+    <BaseSelect :selected="selectedActivityId" :options="options" placeholder="Отдых"/>
   </li>
 </template>
